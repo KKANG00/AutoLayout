5a7fd4b25016c085edfa63fe29e46dfc9564e6c5
KKANG00 <wldn7238@naver.com>
2021-08-10 18:08:19 +0900

    1:1 ratio shape, alignment buttons

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayLabel.layer.cornerRadius = 30
+        // button은 자동으로 설정되지만 lable 등은 아래 속성 중 하나를 변경해줘야한다.
+        displayLabel.layer.masksToBounds = true
+//        displayLabel.clipsToBounds = true
     }
 
 
